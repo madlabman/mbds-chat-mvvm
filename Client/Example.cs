6354f568176aa4365baeb1010d3d698034586f7c
chtotonetak <madlabman@mail.ru>
2019-03-07 11:48:21 +0100

    Update Example class

--- Client/Example.cs
+++ Client/Example.cs
@@ -1,3 +1,7 @@
+/*
+ * This class present simple demo of using Protocol.
+ */
+
 using System;
 using System.Windows;
 using ChatApp.Core;
@@ -8,14 +12,13 @@ namespace ChatApp.Client
     {
         public static void Run()
         {
-//            Protocol.SignUp("J", "agent-j", "j_password");
-            
-//            Protocol.SignUp("K", "agent-k", "k_password");
+            Protocol.SignUp("J", "agent-j", "j_password");
+            Protocol.SignUp("K", "agent-k", "k_password");
             Protocol.SignIn("agent-k", "k_password");
             var jDialog = Protocol.LoadDialog("agent-j");
             if (jDialog != null)
             {
-//                Protocol.SendMessageToTheDialog(jDialog, "Hello, space invaders are coming!");
+                Protocol.SendMessageToTheDialog(jDialog, "Hello, space invaders are coming!");
             }
 
             Protocol.SignIn("agent-j", "j_password");
